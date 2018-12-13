0eb702ad13546dda92f3b1a88fbcc7b1b2a7b98c
Water Li <water.li@oocl.com>
2018-12-13 11:24:36 +0800

    add "addLoadEvent" common method

---
+++ public/js/user.js
@@ -0,0 +1,40 @@
+function addLoadEvent(func) {
+    var oldOnload = window.onload;
+    if (typeof window.onload != 'function') {
+        window.onload = func;
+    } else {
+        window.onload = function () {
+            oldOnload();
+            func();
+        }
+    }
+}
+
+addLoadEvent(addUser);
+
+function addUser() {
+    // https://stackoverflow.com/questions/12765431/how-to-send-a-put-request-from-html-form-in-express-and-node
+    $('#userForm').submit(function (e) {
+        e.preventDefault();
+        var url = '/users';
+        $.ajax(url, {
+            type: 'post',
+            // server must response json data
+            // dataType: 'json',
+            headers: {
+                // res.format
+                Accept: 'application/json'
+            },
+            data: $('#userForm').serialize(),
+            success: function (data) {
+                $('#errorDiv').addClass('text-hide');
+                alert(data.success);
+            },
+            error: function (xhr, textStatus, errorThrown) {
+                var errorHtml = template('errorMsg', xhr.responseJSON);
+                var errorDiv = $('#errorDiv');
+                errorDiv.removeClass('text-hide').html(errorHtml);
+            }
+        });
+    })
+}
