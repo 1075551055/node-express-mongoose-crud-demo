46a8afb7b7fa4ab8ffb222d158fd7e862c9af538
Water Li <water.li@oocl.com>
2018-12-17 23:08:46 +0800

    add global 404 & 500 error handling middleware

--- config/routes/userRoute.js
+++ config/routes/userRoute.js
@@ -1,11 +1,44 @@
 const userController = require('../../app/controllers/userController')
 module.exports = function (app) {
+    app.param('id', userController.load);
     //todo: Router using
+    //app.get, post, put, delete will call app.use, they are route middleware.
     app.get('/', userController.index);
     // app.get('/users/add', userController.add);
     app.post('/users', userController.create);
 
-    // app.post('/user', (req, res)=> {
-    //     res.send({'status': -1})
-    // })
+    app.get('/users/:id/edit', userController.edit);
+
+    app.put('/users/:id', userController.update);
+
+    //global error handler middleware: must has 4 params, when call next(error) in other middleware,
+    // it will turn into this middleware and skip other middleware for the same request url
+    app.use(function (err, req, res, next) {
+        // treat as 404
+        if (err.message
+            && (~err.message.indexOf('not found')
+                || (~err.message.indexOf('Cast to ObjectId failed')))) {
+            return next();
+        }
+
+        console.error(err.stack);
+
+        if (err.stack.includes('ValidationError')) {
+            res.status(422).render('422', { error: err.stack });
+            return;
+        }
+
+        // error page
+        res.status(500).render('500', { error: err.stack });
+    })
+
+    //global 404 handler middleware: assume 404 since no middleware responded
+    app.use(function (req, res) {
+        const payload = {
+            url: req.originalUrl,
+            error: 'Page not found'
+        }
+        if (req.accepts().find((value) => value === 'application/json')) return res.status(404).json(payload);
+        res.status(404).render('404', payload);
+    })
 }
